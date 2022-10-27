2f98d7175511e4b2378c9cdf87fb0cd7761d6880
Sean Marley <seanmarley@Seans-MacBook-Air.local>
2022-10-27 17:07:05 +0100

    Finding the length of the last word in a string

---
+++ LengthOfLastWord/LengthOfLastWord.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int lengthOfLastWord(string s) {
+    int lastCharacter = s.size() -1;
+    int count = 0;
+    
+    while(s[lastCharacter] == ' '){
+        lastCharacter--;
+    }
+    
+    for(int i = lastCharacter; i >= 0; i--){
+        if(s[i] != ' '){
+            count++;
+        }
+        if(s[i] == ' '){
+            break;
+        }
+    }
+    
+    return count;
+}
+
+int main(){
+    cout << lengthOfLastWord("This is a test sentence") << endl; //Ans: 8
+    cout << lengthOfLastWord("  This  is  a  test  sentence  ") << endl; //Ans: 8
+}
